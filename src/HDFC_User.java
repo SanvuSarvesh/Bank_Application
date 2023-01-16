61f57f2836c76e9e5aaa347208993643d4374733
sanvu08 <sarveshkr1308@gmail.com>
2023-01-16 15:38:45 +0530

    First Update

--- src/HDFC_User.java
+++ src/HDFC_User.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.UUID;
 
 public class HDFC_User implements BankInterface {
@@ -10,6 +11,47 @@ public class HDFC_User implements BankInterface {
     private static double rateOfInterest;
     // Create getters and setters for all function
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccpuntNo() {
+        return accpuntNo;
+    }
+
+    public void setAccpuntNo(String accpuntNo) {
+        this.accpuntNo = accpuntNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static double getRateOfInterest() {
+        return rateOfInterest;
+    }
+
+    public static void setRateOfInterest(double rateOfInterest) {
+        HDFC_User.rateOfInterest = rateOfInterest;
+    }
+
     public HDFC_User(String name, double balance, String enteredPassword) {
         this.name =  name;
         this.balance = balance;
@@ -19,21 +61,33 @@ public class HDFC_User implements BankInterface {
     }
     @Override
     public double checkBalance() {
-        return 0;
+        return balance;
     }
 
     @Override
     public String addMoney(int amount) {
-        return null;
+        balance+=amount;
+        return "Your amount has been Credited, Your New Balance is "+String.valueOf(balance);
     }
 
     @Override
     public String withdrawMoney(int amount, String enteredPassword) {
-        return null;
+        if(Objects.equals(enteredPassword, password)){
+            if(amount>balance){
+                return "Opps!! Insuficient Money :(";
+            }
+            else{
+                balance-=amount;
+                return "Your amount has been Debited, Your New Balance is "+String.valueOf(balance);
+            }
+        }
+        else
+            return "Wrong password, Try again..";
     }
 
     @Override
     public double calculateInterest(int years) {
-        return 0;
+        double interest = balance*years*rateOfInterest/100;
+        return interest;
     }
 }
